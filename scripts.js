b02750f7280cafe1d952e060e6bf525ff58f3e52
Seres <sunnylu@dyn-160-39-28-63.dyn.columbia.edu>
2015-03-12 04:28:35 -0400

    updated data format

--- scripts.js
+++ scripts.js
@@ -1,30 +1,68 @@
-  
-     
-     google.load("visualization", "1.1", {packages:["bar"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-        ['Country', '2009', '2010'],
+ google.load('visualization', '1.1', {packages: ['bar']});
+    google.setOnLoadCallback(drawChart);
+    
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Country');
+      data.addColumn('number', '2009');
+      data.addColumn('number', '2010');
+
+      data.addRows([
           ['Australia', 3848, 2761],
           ['Canada', 5131, 4000],
           ['United Kingdom', 13763, 11282],
-          ['United States', 30154, 28831],
+          ['United States', 30154, 28831]
+      ]);
 
-]);
-
-        var options = {
-          chart: {
-            title: 'Aid Committed for Developing Countries',
+      var options = {
+        
+         	title: 'Aid Committed for Developing Countries',
             subtitle: 'Comparisons of development assistance offered by four OECD countries',
-    
+   			colors: ['#F5A9BC', '#FA5882'],
+        
+       
+        width: 900,
+        height: 600,
+        hAxis: {
+          title: 'Country',
+        
+          gridlines: {count: 7},
+          textStyle: {
+            fontName: 'Arial',
+            fontSize: 12,
+            color: '#424242',
+            bold: false,
+            italic: false
+          },
+          titleTextStyle: {
+            fontName: 'Arial',
+            fontSize: 12,
+            color: '#424242',
+            bold: false,
+            italic: false
           }
-        };
+        },
+        vAxis: {
+          title: 'Aid Commited in Thousands',
+          textStyle: {
+            fontSize: 12,
+            color: '#424242',
+            bold: false,
+            italic: false
+          },
+          titleTextStyle: {
+            fontSize: 12,
+            color: '#424242',
+            bold: false,
+            italic: false
+          }
+        }
+      };
 
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('ex3'));
 
-        chart.draw(data, {
- 
- 		colors: ['#F5A9BC', '#FA5882']
- 
-});
-      }
+      chart.draw(data, options);
+    }
+      
