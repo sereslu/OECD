fe3543624748c6599d6b1e1a7e36d45dd70af784
Seres <sunnylu@dyn-209-2-214-204.dyn.columbia.edu>
2015-03-12 00:48:55 -0400

    taking out the data into its own file

---
+++ data.js
@@ -0,0 +1,10 @@
+var oecdData = [
+
+  ['Country', '2009', '2010'],
+          ['Australia', 3848, 2761],
+          ['Canada', 5131, 4000],
+          ['United Kingdom', 13763, 11282],
+          ['United States', 30154, 28831]
+]
+
+
